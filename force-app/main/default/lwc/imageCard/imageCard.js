50c4c7a22445a6d838280e6d4f6326aeb21a0a5b
lopinni <lopik98@gmail.com>
2023-03-17 12:33:36 +0100

    Added main image sync

--- force-app/main/default/lwc/imageCard/imageCard.js
+++ force-app/main/default/lwc/imageCard/imageCard.js
@@ -1,6 +1,14 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { publish, MessageContext } from 'lightning/messageService';
+
+import updateMainImage from "@salesforce/apex/WH_LocationController.updateMainImage";
+import UPDATE_MAIN_IMAGE_CHANNEL from '@salesforce/messageChannel/Update_Main_Image__c';
 
 export default class ImageCard extends LightningElement {
+
+    @wire(MessageContext)
+    messageContext;
 
     @api file;
     @api recordId;
@@ -46,5 +54,29 @@ export default class ImageCard extends LightningElement {
         }
         }
         return "doctype:image";
+    }
+
+    setMainImage() {
+        updateMainImage({
+            recordId: this.recordId,
+            url: this.thumbnail
+        }).then(() => {
+            const payload = { 
+                image: this.thumbnail
+            };
+            publish(this.messageContext, UPDATE_MAIN_IMAGE_CHANNEL, payload);
+        }).then(() => {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Success',
+                message: 'Main image set',
+                variant: 'success'
+            }));
+        }).catch((error) => {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error',
+                message: error,
+                variant: 'error'
+            }));
+        });
     }
 }
--- force-app/main/default/lwc/mainImage/mainImage.js
+++ force-app/main/default/lwc/mainImage/mainImage.js
@@ -1,7 +1,13 @@
 import { LightningElement, api, wire, track } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+
+import UPDATE_MAIN_IMAGE_CHANNEL from '@salesforce/messageChannel/Update_Main_Image__c';
 import getURL from "@salesforce/apex/WH_LocationController.getURL";
 
 export default class MainImage extends LightningElement {
+
+    @wire(MessageContext)
+    messageContext;
 
     @api recordId;
     @track URL;
@@ -15,5 +21,21 @@ export default class MainImage extends LightningElement {
         } else if (error) {
             console.log(error);
         }
+    }
+
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext,
+            UPDATE_MAIN_IMAGE_CHANNEL,
+            (message) => this.handleMessage(message)
+        );
+    }
+
+    handleMessage(message) {
+        this.URL = message.image;
+    }
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
     }
 }
