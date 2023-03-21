7022f19ac09fd63f9e2324bd6ace21e43e678852
lopinni <lopik98@gmail.com>
2023-03-21 19:24:27 +0100

    Added search by other fields

--- force-app/main/default/lwc/productBrowser/productBrowser.js
+++ force-app/main/default/lwc/productBrowser/productBrowser.js
@@ -1,16 +1,29 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import { loadStyle } from "lightning/platformResourceLoader";
 import { getRecord } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
 import RoleName from '@salesforce/schema/User.UserRole.Name';
-import { loadStyle } from "lightning/platformResourceLoader";
 import noHeader from '@salesforce/resourceUrl/NoHeaderStylesheet';
+import getProducts from "@salesforce/apex/WH_LocationController.getProducts";
 
 export default class ProductBrowser extends LightningElement {
 
     userRoleName;
+
     isHousingSales = false;
     isBusinessSales = false;
-    renderPagination = true;
+
+    limit = "10";
+    offset = "0";
+    renderPagination = false;
+
+    productName;
+    productCity;
+    productStreet;
+    productCountry;
+
+    @track
+    products;
 
     constructor() {
         super();
@@ -22,7 +35,9 @@ export default class ProductBrowser extends LightningElement {
         if (error) {
             this.error = error;
         } else if (data) {
-            this.userRoleName = data.fields.UserRole.value.fields.Name.value;
+            if (data.fields.UserRole.value != null) {
+                this.userRoleName = data.fields.UserRole.value.fields.Name.value;
+            }
             if(this.userRoleName == "Business Premises Sales") {
                 this.isBusinessSales = true;
             } else if(this.userRoleName == "Housing Sales") {
@@ -31,8 +46,44 @@ export default class ProductBrowser extends LightningElement {
         }
     }
 
-    handleSearch() {
+    queryProducts() {
+        getProducts({
+            name: this.productName,
+            city: this.productCity,
+            street: this.productStreet,
+            country: this.productCountry,
+            paginationLimit: this.limit,
+            paginationOffset: this.offset
+        }).then(result => {
+            this.products = result; //copy JSON
+            this.renderPagination = true;
+        }).catch(error => {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error',
+                message: error,
+                variant: 'error'
+            }));
+        });
+    }
 
+    setName(event) {
+        this.productName = event.detail.value;
+        this.queryProducts();
+    }
+
+    setCity(event) {
+        this.productCity = event.detail.value;
+        this.queryProducts();
+    }
+
+    setStreet(event) {
+        this.productStreet = event.detail.value;
+        this.queryProducts();
+    }
+
+    setCountry(event) {
+        this.productCountry = event.detail.value;
+        this.queryProducts();
     }
 
 }
