2d34baf4de3d90c01e2f8210acbb04bad9260378
lopinni <lopik98@gmail.com>
2023-03-21 22:08:45 +0100

    card click moves to record page

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,8 +1,19 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class ProductCard extends LightningElement {
+export default class ProductCard extends NavigationMixin(LightningElement) {
 
     @api product;
 
+    navigateToRecordPage() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.product.Id,
+                objectApiName: 'Product2',
+                actionName: 'view'
+            }
+        });
+    }
 
 }
