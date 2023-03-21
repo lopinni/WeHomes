9a7208fa64826155d09a1082f0ea204e8167e897
lopinni <lopik98@gmail.com>
2023-03-21 14:55:28 +0100

    Added initial search functionality

--- force-app/main/default/lwc/productBrowser/productBrowser.js
+++ force-app/main/default/lwc/productBrowser/productBrowser.js
@@ -1,14 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import RoleName from '@salesforce/schema/User.UserRole.Name';
 import { loadStyle } from "lightning/platformResourceLoader";
 import noHeader from '@salesforce/resourceUrl/NoHeaderStylesheet';
 
 export default class ProductBrowser extends LightningElement {
 
+    userRoleName;
+    isHousingSales = false;
+    isBusinessSales = false;
     renderPagination = true;
 
     constructor() {
         super();
         loadStyle(this, noHeader);
+    }
+
+    @wire(getRecord, { recordId: Id, fields: [RoleName] })
+    userDetails({ error, data }) {
+        if (error) {
+            this.error = error;
+        } else if (data) {
+            this.userRoleName = data.fields.UserRole.value.fields.Name.value;
+            if(this.userRoleName == "Business Premises Sales") {
+                this.isBusinessSales = true;
+            } else if(this.userRoleName == "Housing Sales") {
+                this.isHousingSales = true;
+            }
+        }
+    }
+
+    handleSearch() {
+
     }
 
 }
