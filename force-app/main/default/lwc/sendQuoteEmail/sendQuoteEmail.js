e8f7a1f9d28d44a98ee8b3bd5d53a00078cb469d
lopinni <lopik98@gmail.com>
2023-03-31 13:26:25 +0200

    created apex controller for emails

--- force-app/main/default/lwc/sendQuoteEmail/sendQuoteEmail.js
+++ force-app/main/default/lwc/sendQuoteEmail/sendQuoteEmail.js
@@ -1,7 +1,24 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import CONTACT_ID_FIELD from '@salesforce/schema/Quote.ContactId';
+
+const fields = [CONTACT_ID_FIELD];
 
 export default class SendQuoteEmail extends LightningElement {
 
     @api recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields })
+    record;
+
+    get contactId() {
+        return getFieldValue(this.record.data, CONTACT_ID_FIELD);
+    }
+
+    handleClick() {
+        console.log("klik1", this.recordId);
+        console.log("klik2", this.record);
+        console.log("klik3", this.contactId);
+    }
 
 }
