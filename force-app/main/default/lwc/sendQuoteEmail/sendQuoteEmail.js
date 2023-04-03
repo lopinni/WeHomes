8a1e2c44849e81a7e9aa35b15e40eaf8a2872108
lopinni <lopik98@gmail.com>
2023-04-03 10:47:24 +0200

    Added spinner to send email quick action

--- force-app/main/default/lwc/sendQuoteEmail/sendQuoteEmail.js
+++ force-app/main/default/lwc/sendQuoteEmail/sendQuoteEmail.js
@@ -4,9 +4,14 @@ import CONTACT_ID_FIELD from '@salesforce/schema/Quote.ContactId';
 
 import sendQuoteEmail from "@salesforce/apex/WH_SendQuoteController.sendQuoteEmail";
 
+import ERROR from '@salesforce/label/c.Error';
+
 const fields = [CONTACT_ID_FIELD];
 
 export default class SendQuoteEmail extends LightningElement {
+
+    showBottomBox = false;
+    showSuccess = false;
 
     @api recordId;
 
@@ -18,11 +23,19 @@ export default class SendQuoteEmail extends LightningElement {
     }
 
     handleClick() {
+        this.showSuccess = false;
+        this.showBottomBox = true;
         sendQuoteEmail({ 
             quoteId: this.recordId,
             contactId: this.contactId
+        }).then(() => {
+            this.showSuccess = true;
         }).catch(error => {
-            this.showError(error);
+            this.dispatchEvent(new ShowToastEvent({
+                title: ERROR,
+                message: error,
+                variant: 'error'
+            }));
         });
     }
 
