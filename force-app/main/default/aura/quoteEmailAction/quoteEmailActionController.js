971db29300d88a3eadb610a58170d83aef119ba7
lopinni <lopik98@gmail.com>
2023-04-04 13:11:00 +0200

    moved controller logic to helper

--- force-app/main/default/aura/quoteEmailAction/quoteEmailActionController.js
+++ force-app/main/default/aura/quoteEmailAction/quoteEmailActionController.js
@@ -1,14 +1,6 @@
 ({
     handleSuccess : function (cmp, event, helper) {
-        var title = $A.get("$Label.c.Success");
-        var message = $A.get("$Label.c.Offer_Sent");
-        var resultsToast = $A.get("e.force:showToast"); 
-        resultsToast.setParams({ 
-            "title": title, 
-            "message": message,
-            "type": 'success'
-        }); 
-        resultsToast.fire(); 
+        helper.fireEmailSuccessToast();
         $A.get("e.force:closeQuickAction").fire();
     },
 
--- force-app/main/default/aura/quoteEmailAction/quoteEmailActionHelper.js
+++ force-app/main/default/aura/quoteEmailAction/quoteEmailActionHelper.js
@@ -1,5 +1,13 @@
 ({
-    helperMethod : function() {
-
+    fireEmailSuccessToast : function() {
+        var title = $A.get("$Label.c.Success");
+        var message = $A.get("$Label.c.Offer_Sent");
+        var resultsToast = $A.get("e.force:showToast"); 
+        resultsToast.setParams({ 
+            "title": title, 
+            "message": message,
+            "type": 'success'
+        }); 
+        resultsToast.fire(); 
     }
 })
