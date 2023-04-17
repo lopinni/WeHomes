0c377b1cf77bacd650577288c93389b4cdee3799
lopinni <lopik98@gmail.com>
2023-04-17 17:16:02 +0200

    added product parameters on detail page

--- force-app/main/default/lwc/businessPremiseDetail/businessPremiseDetail.js
+++ force-app/main/default/lwc/businessPremiseDetail/businessPremiseDetail.js
@@ -8,20 +8,59 @@ export default class BusinessPremiseDetail extends LightningElement {
     @api recordId;
 
     productName;
+    productDescription;
+    productTotalSurfaceArea;
+    productFloor;
+    productNumberOfFloors;
+    productCity;
+    productStreet;
+    productPostalCode;
+    productCountry;
+    productNumberOfMeetingRooms;
+    productNumberOfKitchens;
+    productParkingSpaces;
+    productFurnished;
+    productWheelchairAccessible;
+    productOpenPlanOffice;
+    productElevatorAccess;
+    productAirConditioning;
 
     productPrice;
 
     connectedCallback() {
-        getProductById({ productId: this.recordId }).then(result => {
-            this.productName = result.Name;
+        getProductById({ productId: this.recordId })
+        .then(result => {
+            this.populateFields(result);
         }).catch(error => {
             console.log(error);
         });
-        getBusinessProductPriceById({ productId: this.recordId }).then(result => {
+        getBusinessProductPriceById({ productId: this.recordId })
+        .then(result => {
             this.productPrice = result.UnitPrice;
         }).catch(error => {
             console.log(error);
         });
+    }
+
+    populateFields(result) {
+        console.log(result);
+        this.productName = result.Name;
+        this.productDescription = result.Description;
+        this.productTotalSurfaceArea = result.Total_surface_area__c;
+        this.productFloor = result.Floor__c;
+        this.productNumberOfFloors = result.Number_of_floors__c;
+        this.productCity = result.City__c;
+        this.productStreet = result.Street__c;
+        this.productPostalCode = result.Postal_Code__c;
+        this.productCountry = result.Country__c;
+        this.productNumberOfMeetingRooms = result.Number_of_meeting_rooms__c;
+        this.productNumberOfKitchens = result.Number_of_kitchens__c;
+        this.productParkingSpaces = result.Parking_spaces__c;
+        this.productFurnished = result.Furnished__c;
+        this.productWheelchairAccessible = result.Wheelchair_accessible__c;
+        this.productOpenPlanOffice = result.Open_plan_office__c;
+        this.productElevatorAccess = result.Elevator_access__c;
+        this.productAirConditioning = result.Air_conditioning__c;
     }
 
 }
