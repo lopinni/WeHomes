0605efa06dddc4a6f1d0e6fa3426bc6eadd84f6f
lopinni <lopik98@gmail.com>
2023-04-17 12:12:56 +0200

    added product details

--- force-app/main/default/lwc/businessPremiseDetail/businessPremiseDetail.js
+++ force-app/main/default/lwc/businessPremiseDetail/businessPremiseDetail.js
@@ -1,5 +1,25 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+
+import getProductById from "@salesforce/apex/WH_LocationController.getProductById";
+
+import ERROR from '@salesforce/label/c.Error';
 
 export default class BusinessPremiseDetail extends LightningElement {
+
     @api recordId;
+
+    productName;
+
+    @wire(getProductById, { productId: '$recordId' })
+    getProductInfo({ error, data }) {
+        if (error) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: ERROR,
+                message: error,
+                variant: 'error'
+            }));
+        } else if (data) {
+            this.productName = data.Name;
+        }
+    }
 }
