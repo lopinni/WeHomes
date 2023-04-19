17173a8f8f67c2840a8be22163c294805b70d87a
lopinni <lopik98@gmail.com>
2023-04-19 14:44:40 +0200

    added footer component

---
+++ force-app/main/default/lwc/customFooter/customFooter.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+import WEHOMES_LOGO from '@salesforce/resourceUrl/WeHomesLogo';
+
+export default class CustomFooter extends LightningElement {
+
+    logoUrl = WEHOMES_LOGO;
+
+}
