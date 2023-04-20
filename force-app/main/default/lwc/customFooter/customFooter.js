82a25118c53e2946008db0fc265c59e0c72a7815
lopinni <lopik98@gmail.com>
2023-04-20 11:13:38 +0200

    added labels to footer

--- force-app/main/default/lwc/customFooter/customFooter.js
+++ force-app/main/default/lwc/customFooter/customFooter.js
@@ -2,8 +2,20 @@ import { LightningElement } from 'lwc';
 
 import WEHOMES_LOGO from '@salesforce/resourceUrl/WeHomesLogo';
 
+import CONTACT_INFORMATION from '@salesforce/label/c.Contact_Information';
+import EMAIL from '@salesforce/label/c.Email';
+import PHONE from '@salesforce/label/c.Phone';
+import COPYRIGHT from '@salesforce/label/c.Copyright';
+
 export default class CustomFooter extends LightningElement {
 
     logoUrl = WEHOMES_LOGO;
+
+    labels = {
+        CONTACT_INFORMATION,
+        EMAIL,
+        PHONE,
+        COPYRIGHT
+    };
 
 }
