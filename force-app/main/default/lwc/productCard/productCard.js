43b29b98efd06b37e0a899e711530d4d55457eaf
lopinni <lopik98@gmail.com>
2023-04-21 10:24:16 +0200

    added missing labels

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -4,7 +4,29 @@ import { NavigationMixin } from 'lightning/navigation';
 import getURL from "@salesforce/apex/WH_LocationController.getURL";
 import getBusinessProductPriceById from "@salesforce/apex/WH_PricebookManagerController.getBusinessProductPriceById";
 
+import LOADING from '@salesforce/label/c.Loading';
+import TOTAL_SURFACE_AREA from '@salesforce/label/c.Total_Surface_Area';
+import FURNISHED from '@salesforce/label/c.Furnished';
+import PRICE from '@salesforce/label/c.Price';
+import WHEELCHAIR_ACCESSIBLE from '@salesforce/label/c.Wheelchair_Accessible';
+import ELEVATOR_ACCESS from '@salesforce/label/c.Elevator_Access';
+import AIR_CONDITIONING from '@salesforce/label/c.Air_Conditioning';
+import YES from '@salesforce/label/c.Yes';
+import NO from '@salesforce/label/c.No';
+
 export default class ProductCard extends NavigationMixin(LightningElement) {
+
+    labels = {
+        LOADING,
+        TOTAL_SURFACE_AREA,
+        FURNISHED,
+        PRICE,
+        WHEELCHAIR_ACCESSIBLE,
+        ELEVATOR_ACCESS,
+        AIR_CONDITIONING,
+        YES,
+        NO
+    };
 
     @api product;
     @track URL;
@@ -17,7 +39,6 @@ export default class ProductCard extends NavigationMixin(LightningElement) {
 
     connectedCallback() {
         getBusinessProductPriceById({ productId: this.product.Id }).then(result => {
-            console.log(result);
             this.productPrice = result.UnitPrice;
             this.loaded = true;
         }).catch(error => {
