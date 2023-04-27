3d446eb47f9b31537a97901e8bb8ae5a011b1487
lopinni <lopik98@gmail.com>
2023-04-27 10:13:06 +0200

    added agent info

--- force-app/main/default/lwc/officeJourneyMenu/officeJourneyMenu.js
+++ force-app/main/default/lwc/officeJourneyMenu/officeJourneyMenu.js
@@ -1,24 +1,50 @@
 import { LightningElement, api, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import getLoggedInStatus from "@salesforce/apex/WH_ReservationController.getLoggedInStatus";
+import getOfficeAgentInfo from "@salesforce/apex/WH_ReservationController.getOfficeAgentInfo";
+
+import ERROR from '@salesforce/label/c.Error';
+import LOADING from '@salesforce/label/c.Loading';
 
 export default class OfficeJourneyMenu extends LightningElement {
+
+    labels = {
+        LOADING
+    }
 
     @api recordId;
 
     loaded = false;
-
-    userRoleName;
     isLoggedIn = false;
+
+    agent;
+
+    showError(error) {
+        console.log(error);
+        this.dispatchEvent(new ShowToastEvent({
+            title: ERROR,
+            message: error.statusText,
+            variant: 'error'
+        }));
+    }
 
     @wire(getLoggedInStatus)
     userDetails({ error, data }) {
         if (error) {
-            console.log(error);
+            this.showError(error);
         } else if (data) {
             this.isLoggedIn = data.IsCurrent;
         }
-        this.loaded = true;
+    }
+
+    connectedCallback() {
+        getOfficeAgentInfo({ productId: this.recordId }).then(result => {
+            this.agent = result;
+            this.loaded = true;
+        }).catch(error => {
+            this.showError(error);
+        });
     }
 
 }
