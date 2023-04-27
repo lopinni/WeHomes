3fecbc4b80e4e9bece2f804e4a6e8aa960e6cbec
lopinni <lopik98@gmail.com>
2023-04-27 08:06:53 +0200

    added check for logged in users

--- force-app/main/default/lwc/officeJourneyMenu/officeJourneyMenu.js
+++ force-app/main/default/lwc/officeJourneyMenu/officeJourneyMenu.js
@@ -1,7 +1,24 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+
+import getLoggedInStatus from "@salesforce/apex/WH_ReservationController.getLoggedInStatus";
 
 export default class OfficeJourneyMenu extends LightningElement {
 
     @api recordId;
+
+    loaded = false;
+
+    userRoleName;
+    isLoggedIn = false;
+
+    @wire(getLoggedInStatus)
+    userDetails({ error, data }) {
+        if (error) {
+            console.log(error);
+        } else if (data) {
+            this.isLoggedIn = data.IsCurrent;
+        }
+        this.loaded = true;
+    }
 
 }
