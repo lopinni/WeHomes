809730420d459464ce2b4b152f9e33f28ccc532a
lopinni <lopik98@gmail.com>
2023-05-17 08:16:00 +0200

    added spinner

--- force-app/main/default/aura/communityCaseForm/communityCaseFormController.js
+++ force-app/main/default/aura/communityCaseForm/communityCaseFormController.js
@@ -1,4 +1,10 @@
 ({
+    doInit : function(component, event, helper) {
+        setTimeout(() => {
+            component.set('v.loading', !component.get('v.loading'));
+        }, 1000);
+    },
+
     itemsChange : function(component, event, helper) {           
         var appEvent = $A.get("e.selfService:caseCreateFieldChange");
         appEvent.setParams({
