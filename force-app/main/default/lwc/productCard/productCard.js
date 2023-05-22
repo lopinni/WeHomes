e220d32b3dfb38d5bd52600c8b1bd80aaf7bba8e
lopinni <lopik98@gmail.com>
2023-05-22 22:03:03 +0200

    added info about price change from standard price

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -4,11 +4,11 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import getURL from "@salesforce/apex/WH_LocationController.getURL";
 import getBusinessProductPriceById from "@salesforce/apex/WH_PricebookManagerController.getBusinessProductPriceById";
+import getBusinessProductStandardPriceById from "@salesforce/apex/WH_PricebookManagerController.getBusinessProductStandardPriceById";
 
 import LOADING from '@salesforce/label/c.Loading';
 import TOTAL_SURFACE_AREA from '@salesforce/label/c.Total_Surface_Area';
 import FURNISHED from '@salesforce/label/c.Furnished';
-import PRICE from '@salesforce/label/c.Price';
 import WHEELCHAIR_ACCESSIBLE from '@salesforce/label/c.Wheelchair_Accessible';
 import ELEVATOR_ACCESS from '@salesforce/label/c.Elevator_Access';
 import AIR_CONDITIONING from '@salesforce/label/c.Air_Conditioning';
@@ -22,7 +22,6 @@ export default class ProductCard extends NavigationMixin(LightningElement) {
         LOADING,
         TOTAL_SURFACE_AREA,
         FURNISHED,
-        PRICE,
         WHEELCHAIR_ACCESSIBLE,
         ELEVATOR_ACCESS,
         AIR_CONDITIONING,
@@ -36,13 +35,24 @@ export default class ProductCard extends NavigationMixin(LightningElement) {
     @api isCommunity;
 
     productPrice;
+    productStandardPrice;
 
     loaded = false;
 
     connectedCallback() {
         getBusinessProductPriceById({ productId: this.product.Id }).then(result => {
             this.productPrice = result.UnitPrice;
-            this.loaded = true;
+            getBusinessProductStandardPriceById({ productId: this.product.Id }).then(result => {
+                this.productStandardPrice = result.UnitPrice;
+                this.loaded = true;
+            }).catch(error => {
+                console.log(error);
+                this.dispatchEvent(new ShowToastEvent({
+                    title: ERROR,
+                    message: error.statusText,
+                    variant: 'error'
+                }));
+            });
         }).catch(error => {
             console.log(error);
             this.dispatchEvent(new ShowToastEvent({
